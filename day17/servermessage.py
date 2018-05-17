64e81c36ff8f8ef5d4fff7ea891fee524650cf56
Marina Lenza <marina.lenza.28@gmail.com>
2018-05-16 23:30:18 -0300

    Add Strings

---
+++ day17/servermessage.py
@@ -0,0 +1,8 @@
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+
+# TODO: print a log message using the variables above.
+# The message should have the same format as this one:
+# "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
+print(username + " accessed the site "+url+" at "+timestamp+".")
