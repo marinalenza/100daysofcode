4b7ec791bba407ef7f9780c4c1f3bebcc03e09e8
Marina Lenza <marina.lenza.28@gmail.com>
2018-05-16 23:16:53 -0300

    Bool Main

---
+++ day17/boolean.py
@@ -0,0 +1,8 @@
+sf_population, sf_area = 864816, 231.89
+rio_population, rio_area = 6453682, 486.5
+
+san_francisco_pop_density = sf_population/sf_area
+rio_de_janeiro_pop_density = rio_population/rio_area
+
+# Write code that prints True if San Francisco is denser than Rio, and False otherwise
+print (san_francisco_pop_density > rio_de_janeiro_pop_density)
