6f2e2ae96e2c83564bdc1573cd35219291a61c0b
Marina Lenza <marina.lenza.28@gmail.com>
2018-05-18 19:54:39 -0300

    String Methods

---
+++ day19/stringMethods.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 18 19:53:55 2018
+
+@author: Marina
+"""
+
+# Browse the complete list of string methods at:
+# https://docs.python.org/3/library/stdtypes.html#string-methods
+# and try them out here
+name="Marina Lenza"
+print(name.capitalize())
+print(name.isalnum())
+print(name.isspace())
+print(name.lower())
+print(name.upper())
