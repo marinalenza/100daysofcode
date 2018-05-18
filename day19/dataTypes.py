8d4cc1936047ad5c578681f1c45242f90eb081ca
Marina Lenza <marina.lenza.28@gmail.com>
2018-05-18 19:45:22 -0300

    Data Types

---
+++ day19/dataTypes.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 18 19:36:43 2018
+
+@author: Marina
+"""
+
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+total_sales = int(mon_sales)+int(tues_sales)+int(wed_sales)+int(thurs_sales)+int(fri_sales)
+#TODO: Print a string with this format: This week's total sales: xxx
+print("This week's total sales: "+str(total_sales))
+# You will probably need to write some lines of code before the print statement.
