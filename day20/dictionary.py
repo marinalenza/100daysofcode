eb0dc4412cc244846a197c7c0cc2afa79dcff7cd
Marina Lenza <marina.lenza.28@gmail.com>
2018-05-19 19:07:37 -0300

    Dictionary

---
+++ day20/dictionary.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 19 19:02:55 2018
+
+@author: Marina
+"""
+
+# Define a Dictionary, population,
+# that provides information
+# on the world's largest cities.
+# The key is the name of a city
+# (a string), and the associated
+# value is its population in
+# millions of people.
+
+#   Key     |   Value
+# Shanghai  |   17.8
+# Istanbul  |   13.3
+# Karachi   |   13.0
+# Mumbai    |   12.5
+population = {'Shanghai':17.8 , 'Istanbul':13.3 , 'Karachi':13.0 , 'Mumbai':12.5}
+print(population['Mumbai'])
