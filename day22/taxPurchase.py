9b455b4a4cd5c5d274b86352391efdf73f99a9f7
Marina Lenza <marina.lenza.28@gmail.com>
2018-05-21 16:38:47 -0300

    Tax Purchase - this would be so much easy with switch/case

---
+++ day22/taxPurchase.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 21 16:36:22 2018
+
+@author: Marina
+"""
+
+# '''
+# Depending on where an individual is from we need to tax them 
+# appropriately.  The states of CA, MN, and 
+# NY have taxes of 7.5%, 9.5%, and 8.9% respectively.
+# Use this information to take the amount of a purchase and 
+# the corresponding state to assure that they are taxed by the right
+# amount.
+# '''
+state = 'CA'
+purchase_amount = 130
+
+if state == 'CA':
+    tax_amount = .075
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state == 'MN':
+    tax_amount = .095
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state == 'NY':
+    tax_amount = .089
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+print(result)
