71ff8604f0405f40d0725540fd9652a978e9c1d5
Marina Lenza <marina.lenza.28@gmail.com>
2018-05-27 21:20:11 -0300

    Factorial with for loop

---
+++ day27/forFac.py
@@ -0,0 +1,20 @@
+# This is the number we'll find the factorial of - change it to test your code!
+
+number = 6
+
+# We'll start with the product equal to the number
+
+product = number
+
+
+#Write a for loop that calculates the factorial of our number 
+
+for i in range(1,number):
+
+    product *= i
+
+
+
+# print the factorial of your number
+
+print(product)
