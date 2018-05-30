9d473e7d260a2e8343fcf39d1cb97cf73ceb7a33
Marina Lenza <marina.lenza.28@gmail.com>
2018-05-29 22:34:56 -0300

    Count by

---
+++ day29/countby.py
@@ -0,0 +1,18 @@
+start_num = 1
+
+end_num = 23
+
+count_by = 1
+
+break_num = start_num
+
+# write a while loop that uses break_num as the ongoing number to 
+
+#   check against end_num
+
+while(break_num != end_num):
+
+    break_num += count_by
+
+
+print(break_num)
